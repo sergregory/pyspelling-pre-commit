535e1b8e0406648914a507552b61183b98cc93db
Grigory Serebryakov <serebryakovgr@gmail.com>
2020-05-07 12:28:21 +0300

    Mirror: 1.0

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['pyspelling==1.0b2'],
+    install_requires=['pyspelling==1.0'],
 )
