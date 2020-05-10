a736402623bbf6d7b73fcc4f9097219b5c661571
Grigory Serebryakov <serebryakovgr@gmail.com>
2020-05-10 19:08:40 +0300

    Fix setup

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='mirrors-pyspelling-for-pre-commit',
     version='0.0.0',
     install_requires=['pyspelling==2.6'],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     entry_points = {
-        'console_scripts': ['wraps_pyspelling=wraps_pyspelling:entrypoint'],
+        'console_scripts': ['wraps_pyspelling=mirrors_pyspelling.wraps_pyspelling:entrypoint'],
     }
 )
