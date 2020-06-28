b0f7995809b4bc7f876585a1b8ce7dc210f1e95d
pre-commit <pre-commit@example.com>
2020-06-28 08:03:35 +0000

    Mirror: 2.6.1

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
-    name='mirrors-pyspelling-for-pre-commit',
+    name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['pyspelling==2.6'],
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    entry_points = {
-        'console_scripts': ['wraps_pyspelling=mirrors_pyspelling.wraps_pyspelling:entrypoint'],
-    }
+    install_requires=['pyspelling==2.6.1'],
 )
